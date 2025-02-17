4aab3253691480e56639d4132b36816230223880
uday-2425 <chuday2425@gmail.com>
2025-02-17 21:02:24 +0530

    Add files via upload
    🚀 Day 17: Java Challenge - Number Classification! 🎯
    
    ✅ Today, I built a simple number classification program in Java! This program takes user input and determines whether the number is positive, negative, or zero using conditional statements.
    
    🔑 Key Insights:
    🔹 Used Scanner to take user input 📥
    🔹 Implemented if-else for efficient condition checking ⚡
    🔹 Ensured clear and precise output based on the input value 🛠️
    
    💡 Optimization Tip: Always close the Scanner after use to prevent resource leaks!
    
    📌 Follow my journey as I continue this 100-day Java challenge! 💻🔥
    
    #100DaysOfCode #Java #CodingChallenge #GitHub #CodeEveryday #Programming #JavaDeveloper #Tech #CodeNewbie #LearnToCode #Developer #CodingLife #TechCommunity #CodeWithMe #JavaProgramming 🚀

---
+++ Day17.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Day17{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+System.out.print("input number:");
+int input=sc.nextInt();
+if(input>0){
+System.out.print("number is positive ");
+}
+else if(input<0){
+System.out.print(" number is negative");
+}
+else{
+System.out.print(" number is zero");
+}
+}
+} 
